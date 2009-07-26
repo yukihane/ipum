79599913e5746e13ea38c90d0e6827f8cc37f376
yuki <yuki@7fd9e3a5-e8e0-ae49-998b-59206cbea903>
2009-07-26 10:15:06 +0000

    dnd実装
    git-svn-id: http://192.168.11.7/svn/ipum/ipum/trunk@3 7fd9e3a5-e8e0-ae49-998b-59206cbea903

--- src/yukihane/ipum/MainWindow.java
+++ src/yukihane/ipum/MainWindow.java
@@ -1,15 +1,18 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * MainWindow.java
- *
- * Created on 2009/07/26, 18:18:15
- */
+/* $Id$ */
 package yukihane.ipum;
 
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetAdapter;
+import java.awt.dnd.DropTargetDragEvent;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.dnd.DropTargetListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
@@ -21,9 +24,47 @@ import javax.swing.UIManager;
  */
 public class MainWindow extends javax.swing.JFrame {
 
+    private final MyTableModel model = new MyTableModel();
+
     /** Creates new form MainWindow */
     public MainWindow() {
         initComponents();
+
+        DropTargetListener dtl = new DropTargetAdapter() {
+
+            @Override
+            public void dragOver(DropTargetDragEvent dtde) {
+                if (dtde.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+                    dtde.acceptDrag(DnDConstants.ACTION_COPY);
+                    return;
+                }
+                dtde.rejectDrag();
+            }
+
+            public void drop(DropTargetDropEvent dtde) {
+                try {
+                    if (dtde.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+                        dtde.acceptDrop(DnDConstants.ACTION_COPY);
+                        Transferable transferable = dtde.getTransferable();
+                        List list = (List) transferable.getTransferData(DataFlavor.javaFileListFlavor);
+                        for (Object o : list) {
+                            if (o instanceof File) {
+                                File file = (File) o;
+                                model.addTest(new Test(file.getName(), file.getAbsolutePath()));
+                            }
+                        }
+                    }
+                } catch (IOException ex) {
+                    Logger.getLogger(MainWindow.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (UnsupportedFlavorException ex) {
+                    Logger.getLogger(MainWindow.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                dtde.rejectDrop();
+            }
+        };
+
+        new DropTarget(mainTable, DnDConstants.ACTION_COPY, dtl, true);
+        new DropTarget(scrollPane.getViewport(), DnDConstants.ACTION_COPY, dtl, true);
     }
 
     /** This method is called from within the constructor to
@@ -35,26 +76,16 @@ public class MainWindow extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        buttonProperty = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        propertyButton = new javax.swing.JButton();
+        scrollPane = new javax.swing.JScrollPane();
+        mainTable = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        buttonProperty.setText("Property");
+        propertyButton.setText("Property");
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ));
-        jScrollPane1.setViewportView(jTable1);
+        mainTable.setModel(model);
+        scrollPane.setViewportView(mainTable);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -63,17 +94,17 @@ public class MainWindow extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 277, Short.MAX_VALUE)
-                    .addComponent(buttonProperty))
+                    .addComponent(scrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 277, Short.MAX_VALUE)
+                    .addComponent(propertyButton))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(buttonProperty)
+                .addComponent(propertyButton)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 253, Short.MAX_VALUE)
+                .addComponent(scrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 253, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -116,8 +147,8 @@ public class MainWindow extends javax.swing.JFrame {
         }
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton buttonProperty;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JTable mainTable;
+    private javax.swing.JButton propertyButton;
+    private javax.swing.JScrollPane scrollPane;
     // End of variables declaration//GEN-END:variables
 }
